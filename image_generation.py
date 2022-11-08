3fc70404a17f9ea29dca2cae8ef2f9be42f0c4be
VisibleReality <visible@duck.com>
2022-11-08 21:58:35 +1100

    Add image_generation.py

---
+++ image_generation.py
@@ -0,0 +1,56 @@
+import random
+
+import requests
+from lxml import etree
+
+
+def get_random_image_from_web (output_location: str) -> None:
+	"""
+	Gets a random image from the artistic/anime category on besthdwallpaper.com.
+	:param output_location: The path to save the downloaded image to
+	:return: None
+	"""
+	# Get page information
+	page_info: dict[str, any] = requests.get \
+		("https://www.besthdwallpaper.com/anime-wallpapers-ct_en-US-51/1?isJson=true").json()["pagingInfo"]
+
+	last_page = page_info["pageCount"]
+
+	# Loop until we successfully get an image
+	got_image = False
+
+	while not got_image:
+		# Pick a random page
+		page_number = random.randint(1, last_page)
+
+		# Download page JSON
+		page_data: list[dict[str, any]] = requests.get \
+			(f"https://www.besthdwallpaper.com/anime-wallpapers-ct_en-US-51/{page_number}?isJson=true").json()["data"]
+
+		# Pick random image
+		image_info: dict[str, any] = random.choice(page_data)
+
+		# Check if image meets conditions
+		# noinspection PyChainedComparisons
+		if not image_info["isOver18"] and \
+				image_info["imWidth"] >= 3840 and \
+				image_info["imHeight"] >= 2160 and \
+				image_info["imWidth"] >= image_info["imHeight"]:
+			# Get image details page
+			image_details = requests.get(f"https://www.besthdwallpaper.com{image_info['detailPageUrl']}")
+
+			# Get link to image
+			# noinspection PyProtectedMember
+			elements: list[etree._Element] = etree.HTML(image_details.text).xpath('//a[text()=" 3840x2160 "]')
+			if len(elements) != 0:
+				image_url: str = elements[0].attrib["href"]
+
+				# Check if image is a jpeg
+				if image_url.endswith(".jpg") or image_url.endswith(".jpeg") or image_url.endswith(".jpe") or \
+						image_url.endswith(".jif") or image_url.endswith(".jfif") or image_url.endswith(".jfi"):
+					# Download file
+					with open(output_location, "wb") as output_file:
+						output_file.write(requests.get(image_url).content)
+
+					# Finish loop
+					got_image = True
