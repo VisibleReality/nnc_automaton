962bba6d0657f9aa09a5282892498b36c9fc36f6
VisibleReality <visible@duck.com>
2024-04-26 23:29:32 +1000

    update image generation to work with the website

--- image_generation.py
+++ image_generation.py
@@ -3,6 +3,10 @@ import random
 import requests
 from lxml import etree
 
+headers = {
+	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+}
+
 
 def get_random_image_from_web (output_location: str) -> None:
 	"""
@@ -12,7 +16,8 @@ def get_random_image_from_web (output_location: str) -> None:
 	"""
 	# Get page information
 	page_info: dict[str, any] = requests.get \
-		("https://www.besthdwallpaper.com/anime-wallpapers-ct_en-US-51/1?isJson=true").json()["pagingInfo"]
+		("https://www.besthdwallpaper.com/anime-wallpapers-ct_en-US-51/1?isJson=true",
+		 headers = headers).json()["psf"]
 
 	last_page = page_info["pageCount"]
 
@@ -25,7 +30,8 @@ def get_random_image_from_web (output_location: str) -> None:
 
 		# Download page JSON
 		page_data: list[dict[str, any]] = requests.get \
-			(f"https://www.besthdwallpaper.com/anime-wallpapers-ct_en-US-51/{page_number}?isJson=true").json()["data"]
+			(f"https://www.besthdwallpaper.com/anime-wallpapers-ct_en-US-51/{page_number}?isJson=true",
+			 headers = headers).json()["data"]
 
 		# Pick random image
 		image_info: dict[str, any] = random.choice(page_data)
@@ -37,7 +43,8 @@ def get_random_image_from_web (output_location: str) -> None:
 				image_info["imHeight"] >= 2160 and \
 				image_info["imWidth"] >= image_info["imHeight"]:
 			# Get image details page
-			image_details = requests.get(f"https://www.besthdwallpaper.com{image_info['detailPageUrl']}")
+			image_details = requests.get(f"https://www.besthdwallpaper.com{image_info['detailPageUrl']}",
+										 headers = headers)
 
 			# Get link to image
 			# noinspection PyProtectedMember
@@ -50,7 +57,11 @@ def get_random_image_from_web (output_location: str) -> None:
 						image_url.endswith(".jif") or image_url.endswith(".jfif") or image_url.endswith(".jfi"):
 					# Download file
 					with open(output_location, "wb") as output_file:
-						output_file.write(requests.get(image_url).content)
+						output_file.write(requests.get(image_url, headers = headers).content)
 
 					# Finish loop
 					got_image = True
+
+
+if __name__ == "__main__":
+	get_random_image_from_web(output_location = r"C:\Users\ajdmi\PycharmProjects\nnc_automaton\data\test.jpg")
